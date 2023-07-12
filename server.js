e8c61e32512438dcd042ac6b6d527e89df0ab8d8
Marcus K <marcus@SE-5CG9372GNZ>
2023-07-12 15:14:37 +0200

    updated so tables lists on button click

---
+++ server.js
@@ -0,0 +1,39 @@
+console.log('Server-side code running');
+
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+const aws = require("@aws-sdk/client-dynamodb");
+
+const REGION = "eu-west-1"; //e.g. "us-east-1"";
+const dynamodbClient = new aws.DynamoDB({ region: REGION });
+
+app.set('view engine', 'ejs');
+var port = process.env.PORT || 3000;
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/bye", function(req, res){
+  res.render("bye"); 
+});
+
+app.listen(port, function(){
+     console.log("server is running on port" + port);
+});
+
+app.post('/clicked', (req, res) => {
+    const click = {clickTime: new Date()};
+    console.log("redirected to function clicked");
+  
+    dynamodbClient.listTables({}, (err, data)=>{
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(data.TableNames);
+        }
+    });
+  });
